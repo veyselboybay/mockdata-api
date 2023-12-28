9dcf074731d04ffdd0f927e6900d7bb3e7d83e07
veyselboybay <veysel.boybay@outlook.com>
2023-12-28 02:36:32 -0500

    RateLimiter and mock data generator are added

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const helmet = require('helmet')
 require('dotenv').config()
 const authRouter = require('./routes/auth_route')
+const dataRouter = require('./routes/data_route')
+const { authMiddleware } = require('./middlewares/auth_middleware')
+const { rateLimiter } = require('./middlewares/ratelimit')
 
 const app = express()
 
 const PORT = process.env.PORT || 3000;
 
+let counter = 0;
+
 // app middlewares
 app.use(cors())
+app.use(helmet())
 app.use(express.json())
+app.use((req,res,next) => {
+    counter = counter + 1;
+    next()
+});
 
-app.use('/api/v1', authRouter)
+app.get('/counter', (req, res) => {
+    return res.json({counter:counter})
+})
+
+app.use('/api/v1', authRouter);
+app.use('/api/v1', [authMiddleware,rateLimiter], dataRouter);
+
+// Health check
+app.use('/healthz', (req, res) => {
+    try {
+        return res.status(200).json({"message": 'Healthy'});
+    } catch (error) {
+        return res.status(500).json({success:false,msg:error.message})
+    }
+})
 
 // db connection
 mongoose.connect(process.env.DB_CONNECTION).then(() => {
